a6e7579417d567a0f6f0f2ab6c5f8f39a5b14eab
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-20 21:32:11 +0800

    added

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/newticket', function () {
-    return view('ticket');
+Route::get('/', function () {
+    return view('ticket.index');
 });
